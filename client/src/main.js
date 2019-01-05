a4dda100583a451cc1bef91ebdf60aac353e35ad
takei.shg <takei.shg@gmail.com>
2019-01-06 01:24:06 +0900

    feat: add firebase project settings

--- client/src/main.js
+++ client/src/main.js
@@ -3,6 +3,7 @@ import Vue from 'vue';
 import App from './App.vue';
 import router from './router';
 import store from './store';
+import '@/firebase/';
 import Buefy from 'buefy';
 import 'buefy/dist/buefy.css';
 
