2bc4f018b37a46394f62cf817fc3481bf7ea5754
takei.shg <takei.shg@gmail.com>
2019-01-08 16:08:48 +0900

    chore: vue add @vue/pwa

--- client/src/main.js
+++ client/src/main.js
@@ -5,6 +5,7 @@ import App from './App.vue';
 import router from './router';
 import store from './store';
 import '@/firebase/';
+import './registerServiceWorker'
 
 Vue.config.productionTip = false;
 
