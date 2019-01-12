bbb1f68eb865eae8aad0f659d5d37a1b796ae902
takei.shg <takei.shg@gmail.com>
2019-01-12 18:24:44 +0900

    fix: remove footer.vue

--- client/src/App.vue
+++ client/src/App.vue
@@ -5,20 +5,16 @@
     <v-content transition="slide-x-transition">
       <router-view></router-view>
     </v-content>
-
-    <home-footer></home-footer>
   </v-app>
 </template>
 
 <script>
 import AppNavigation from '@/components/AppNavigation';
-import HomeFooter from '@/components/Footer';
 
 export default {
   name: 'App',
   components: {
-    AppNavigation,
-    HomeFooter
+    AppNavigation
   },
   data () {
     return {
