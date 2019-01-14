bf783c8fa47f13a59589001f4621375547bc78dd
takei.shg <takei.shg@gmail.com>
2019-01-14 22:34:27 +0900

    feat: use md5 url and store record on firestore/posts

--- client/src/store.js
+++ client/src/store.js
@@ -4,6 +4,8 @@ import router from '@/router';
 import firebase from 'firebase/app';
 import 'firebase/auth';
 import 'firebase/storage';
+import 'firebase/firestore';
+import md5 from 'md5';
 
 Vue.use(Vuex);
 
@@ -29,9 +31,37 @@ export default new Vuex.Store({
     upload({ state }, { file, fileName, description }) {
       const storageRef = firebase.storage().ref();
       const uid = state.user.uid;
-      const fileRef = storageRef.child(`posts/${uid}/${fileName}`);
+      const hash = md5(uid + Date.now().toString());
+      const [x, extention] = fileName.split('.');
+      const imageStorePath = `posts/${hash}/image.${extention}`
+      const fileRef = storageRef.child(imageStorePath);
       fileRef.put(file).then(() => {
         console.log('Uploaded a blob or file!');
+        const db = firebase.firestore;
+        const firestore = firebase.firestore();
+        const settings = {
+          timestampsInSnapshots: true
+        };
+        firestore.settings(settings);
+
+        firestore.collection('posts').add({
+          author: {
+            people_ref: `people/${uid}`,
+            uid: uid
+          },
+          image_url: imageStorePath,
+          text: description,
+          thumb_image_url: '',
+          title: '',
+          created_at: Date.now()/1000,
+          updated_at: Date.now()/1000
+        })
+        .then(function(docRef) {
+          console.log("Document written with ID: ", docRef.id);
+        })
+        .catch(function(error) {
+            console.error("Error adding document: ", error);
+        });
       });
     },
     onAuthStateChanged({ commit }) {
