777f39ab80aa924fe32c041999bb65319d010004
takei.shg <takei.shg@gmail.com>
2019-01-15 21:58:30 +0900

    chore: npm run lint

--- client/src/store.js
+++ client/src/store.js
@@ -30,30 +30,32 @@ export default new Vuex.Store({
       const uid = state.user.uid;
       const hash = md5(uid + Date.now().toString());
       const [x, extention] = fileName.split('.');
-      const imageStorePath = `posts/${hash}/image.${extention}`
+      const imageStorePath = `posts/${hash}/image.${extention}`;
       const fileRef = storageRef.child(imageStorePath);
       fileRef.put(file).then(() => {
         console.log('Uploaded a blob or file!');
 
-        Firebase.firestore.collection('posts').add({
-          author: {
-            people_ref: `people/${uid}`,
-            uid: uid
-          },
-          image_url: imageStorePath,
-          text: description,
-          thumb_image_url: '',
-          title: '',
-          created_at: Date.now()/1000,
-          updated_at: Date.now()/1000
-        })
-        .then(function(docRef) {
-          console.log("Document written with ID: ", docRef.id);
-          router.push('/timeline');
-        })
-        .catch(function(error) {
-          console.error("Error adding document: ", error);
-        });
+        Firebase.firestore
+          .collection('posts')
+          .add({
+            author: {
+              people_ref: `people/${uid}`,
+              uid: uid
+            },
+            image_url: imageStorePath,
+            text: description,
+            thumb_image_url: '',
+            title: '',
+            created_at: Date.now() / 1000,
+            updated_at: Date.now() / 1000
+          })
+          .then(function(docRef) {
+            console.log('Document written with ID: ', docRef.id);
+            router.push('/timeline');
+          })
+          .catch(function(error) {
+            console.error('Error adding document: ', error);
+          });
       });
     },
     onAuthStateChanged({ commit }) {
@@ -64,37 +66,35 @@ export default new Vuex.Store({
       });
     },
     async userLogin({}, { email, password }) {
-      Firebase
-        .auth
-        .signInWithEmailAndPassword(email, password)
-        .catch(() => {
-          router.push('/');
-        });
+      Firebase.auth.signInWithEmailAndPassword(email, password).catch(() => {
+        router.push('/');
+      });
     },
     userJoin({}, { email, password }) {
-      Firebase
-        .auth
+      Firebase.auth
         .createUserWithEmailAndPassword(email, password)
         .then(credential => {
-          Firebase.firestore.collection('people').doc(credential.user.uid).set({
-            created_at: Date.now()/1000,
-            updated_at: Date.now()/1000
-          })
-          .then(function() {
-            console.log("Document successfully written.");
-            router.push('/about');
-          })
-          .catch(function(error) {
-              console.error("Error adding document: ", error);
-          });
+          Firebase.firestore
+            .collection('people')
+            .doc(credential.user.uid)
+            .set({
+              created_at: Date.now() / 1000,
+              updated_at: Date.now() / 1000
+            })
+            .then(function() {
+              console.log('Document successfully written.');
+              router.push('/about');
+            })
+            .catch(function(error) {
+              console.error('Error adding document: ', error);
+            });
         })
         .catch(() => {
           router.push('/');
         });
     },
     userSignOut() {
-      Firebase
-        .auth
+      Firebase.auth
         .signOut()
         .then(() => {
           router.push('/');
