8a40530cb1076cc85d353eb1e78a2d19c23fa8c5
takei.shg <takei.shg@gmail.com>
2019-01-16 10:49:40 +0900

    feat: japanese menu

--- client/src/views/Signin.vue
+++ client/src/views/Signin.vue
@@ -4,21 +4,21 @@
       <v-flex xs12 sm8 md4>
         <v-card class="elevation-12">
           <v-toolbar dark color="primary">
-            <v-toolbar-title>Login Form</v-toolbar-title>
+            <v-toolbar-title>ログインしますか？</v-toolbar-title>
           </v-toolbar>
           <v-card-text>
             <v-form ref="form" v-model="valid" lazy-validation>
-               <v-text-field prepend-icon="person" name="email" label="Email" type="email"
+               <v-text-field prepend-icon="person" name="email" label="Eメール" type="email"
                              v-model="email" :rules="emailRules" required>
                </v-text-field>
-               <v-text-field prepend-icon="lock" name="password" label="Password" id="password"
+               <v-text-field prepend-icon="lock" name="password" label="パスワード" id="password"
                              type="password" required v-model="password" :rules="passwordRules">
                </v-text-field>
             </v-form>
           </v-card-text>
           <v-card-actions>
             <v-spacer></v-spacer>
-            <v-btn color="primary" :disabled="!valid" @click="submit">Login</v-btn>
+            <v-btn color="primary" :disabled="!valid" @click="submit">ログインする</v-btn>
           </v-card-actions>
         </v-card>
       </v-flex>
